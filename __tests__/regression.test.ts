9bb38a354815342747d922aa43387b2582425dc4
Srđan Prodanović <srdjan.prodanovic@gmail.com>
2026-01-17 06:51:52 +0100

    Add test for pawn moves on edge ranks

--- __tests__/regression.test.ts
+++ __tests__/regression.test.ts
@@ -29,6 +29,15 @@ describe('Regression Tests', () => {
     expect(chess.moves().join(' ')).toBe('Kd2 Ke2 Kxf2 Kf1 Kd1')
   })
 
+  it('Github Issue ##577 - pawn on edge rank should have no moves', () => {
+    const chess = new Chess()
+    chess.clear()
+    chess.put({ type: 'k', color: 'w' }, 'e1')
+    chess.put({ type: 'k', color: 'b' }, 'e8')
+    chess.put({ type: 'p', color: 'w' }, 'h8')
+    expect(chess.moves({ square: 'h8', verbose: true })).toEqual([])
+  })
+
   it('Github Issue #85 (white) - SetUp and FEN should be accepted in loadPgn', () => {
     const chess = new Chess()
     const pgn = [
